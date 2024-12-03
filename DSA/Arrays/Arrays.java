6737d17b3482c542be4f4c14ff96737a140e5118
Aditya <aditya.p.navgire@gmail.com>
2024-12-03 23:09:24 +0530

    Initial java commit

---
+++ DSA/Arrays/Arrays.java
@@ -0,0 +1,14 @@
+package DSA.Arrays;
+
+public class Arrays {
+    public static void main(String[] args)
+    {
+        int[] arr = new int[5];
+        arr[0] = 10;
+        float[] f = {0.5f, 0.6f, 0.7f, 0.8f, 0.9f};
+        System.out.println(arr[0]);
+        System.out.println(f[1]);
+
+    }
+}
+
