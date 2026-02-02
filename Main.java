ac9e4591b764fa125d32b642079fd545229b147a
adityanavgire01 <aditya.p.navgire@gmail.com>
2026-02-02 12:35:00 +0000

    Enter value using BufferedReader (faster than Scanner)

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your number: ");
-        int num = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("You've entered number: " + num);
+        System.out.print("Enter an integer: ");
 
+        int num = Integer.parseInt(reader.readLine());
+
+        System.out.println("Entered num : " + num);
     }
 }
