c4a9e32a6dc7f8d00b008e1dde96b0d8cd4ef20a
adityanavgire01 <aditya.p.navgire@gmail.com>
2026-02-04 13:42:28 +0000

    String being immutable if changed, we can verify the change in hashCode()

--- Main.java
+++ Main.java
@@ -5,11 +5,9 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter name: ");
+        String name = reader.readLine();
 
-        System.out.print("Enter an integer: ");
-
-        int num = Integer.parseInt(reader.readLine());
-
-        System.out.println("Entered num : " + num);
+        System.out.println("Your nams is : " + name);
     }
 }
